b978e4b17f8f7681565daa3de4a4fbbb46b9103e
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-02-09 23:41:52 -0800

    2nd commit

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Layout.module.css'
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+        <main className={styles.main}>
+
+        </main>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>
+        <meta name='keywords' content='web dev, programming'></meta>
+        </title>
+      </Head>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
